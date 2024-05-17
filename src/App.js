253ab360831f823e6bcd6b6973dbb5fdd56aeccc
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-05-17 12:42:11 +0530

    setup a all files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's The Task Title?" />
+          </div>
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's The Task Title?" />
+          </div>
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's The Task Title?" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
